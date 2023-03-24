f04c585e967f28640ba879c42acb240ac9f12082
thezxzx <themasterzxzx@gmail.com>
2023-03-24 16:22:34 -0700

    importar entidad de productos en su modulo para el ORM

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ProductsService } from './products.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
 import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
 
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService]
+  providers: [ProductsService],
+  imports: [
+    TypeOrmModule.forFeature([Product]), // entidades que el modulo esta definiendo
+  ],
 })
 export class ProductsModule {}
