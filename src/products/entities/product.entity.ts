960ac3dcadeff0778da19e74c2f17d772819ace5
thezxzx <themasterzxzx@gmail.com>
2023-03-24 16:30:08 -0700

    Agrega campos en la entidad product

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -13,4 +13,34 @@ export class Product {
     unique: true,
   })
   title: string;
+
+  @Column('numeric')
+  price: number;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  slug: string;
+
+  @Column('int', {
+    default: 0,
+  })
+  stock: number;
+
+  @Column('text', {
+    array: true,
+  })
+  sizes: string[];
+
+  @Column('text')
+  gender: string;
+
+  // tags
+  // images
 }
