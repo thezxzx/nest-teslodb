c85d9a3d724e2f916da6a0cdbce0f3d59d32b5d0
thezxzx <themasterzxzx@gmail.com>
2023-03-25 15:52:16 -0700

    Acutalizar producto y manejo de errores

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -87,8 +87,21 @@ export class ProductsService {
     return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepository.preload({
+      id, // Obtener producto por id
+      ...updateProductDto, // Colocar las propiedades del dto
+    });
+
+    if (!product)
+      throw new NotFoundException(`Product with id: ${id} not found`);
+
+    try {
+      await this.productRepository.save(product);
+      return product;
+    } catch (error) {
+      this.handleDBExceptions(error);
+    }
   }
 
   async remove(id: string) {
