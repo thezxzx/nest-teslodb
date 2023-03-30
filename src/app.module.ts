66df0472ec1edd2f623aab975e6e541843856b77
thezxzx <themasterzxzx@gmail.com>
2023-03-30 01:19:02 -0700

    app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
+import { ProductsModule } from './products/products.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
