d8e3423132ae3471d62668998c7364be73ed809c
thezxzx <themasterzxzx@gmail.com>
2023-04-25 16:37:20 -0700

    tipos para los archivos, helper para filtrar archivos permitidos

---
+++ src/files/files.controller.ts
@@ -0,0 +1,31 @@
+import {
+  BadRequestException,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { FilesService } from './files.service';
+import { fileFilter } from './helpers/fileFilter.helper';
+@Controller('files')
+export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
+  @Post('product')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+    }),
+  )
+  uploadProductImage(@UploadedFile() file: Express.Multer.File) {
+    console.log({ fileInController: file });
+
+    if (!file)
+      throw new BadRequestException('Make sure that the file is an image');
+
+    return {
+      fileName: file.originalname,
+    };
+  }
+}
