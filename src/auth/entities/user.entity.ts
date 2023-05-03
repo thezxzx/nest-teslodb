9826454e75273e87b71cf41a40622240b1ab627d
thezxzx <themasterzxzx@gmail.com>
2023-05-03 16:45:56 -0700

    Crear entidad de Usuarios

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', {
+    unique: true,
+  })
+  email: string;
+
+  @Column('text')
+  password: string;
+
+  @Column('text')
+  fullname: string;
+
+  @Column('bool')
+  isActive: boolean; // No eliminar usuarios físicametne de la base de datos, cambiar el activo a falso
+
+  @Column('text', {
+    array: true,
+    default: ['user'],
+  })
+  roles: string[];
+}
