5fceb22ac698ce36cbb062b7279fd2c5d8c1b9b4
thezxzx <themasterzxzx@gmail.com>
2023-05-05 14:17:00 -0700

    instalar dependencias de jwt y passwport y configrar el passport en el authmodule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -7,7 +10,26 @@ import { User } from './entities/user.entity';
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: { expiresIn: '2h' },
+    // }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // console.log('JWT Secret', configService.get('JWT_SECRET'));
+        // console.log('JWT SECRET', process.env.JWT_SECRET);
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: { expiresIn: '2h' },
+        };
+      },
+    }),
+  ],
   exports: [TypeOrmModule],
 })
 export class AuthModule {}
