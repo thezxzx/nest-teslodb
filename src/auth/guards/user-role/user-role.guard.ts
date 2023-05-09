9a7a7236366a493597794f6b2da8c4b22a4cd4fd
thezxzx <themasterzxzx@gmail.com>
2023-05-08 17:03:53 -0700

    Comparar los roles permitidos con los roles del usuario

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,13 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { User } from 'src/auth/entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -14,9 +21,20 @@ export class UserRoleGuard implements CanActivate {
       context.getHandler(),
     );
 
-    console.log({ validRoles });
+    // Obtener usuario
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
 
-    console.log('UserRoleGuard');
-    return true;
+    if (!user) throw new BadRequestException('User not found');
+
+    for (const roles of user.roles) {
+      if (validRoles.includes(roles)) {
+        return true;
+      }
+    }
+
+    throw new ForbiddenException(
+      `User ${user.fullName} need a valid role: [${validRoles}]`,
+    );
   }
 }
