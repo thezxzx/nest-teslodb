9737dbf8f182c4eec6e2bac0f6c358ae47356a8c
thezxzx <themasterzxzx@gmail.com>
2023-05-15 18:53:43 -0700

    Documentar DTOs

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,13 +1,22 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsOptional, Min } from 'class-validator';
 
 export class PaginationDto {
+  @ApiProperty({
+    default: 10,
+    description: 'How many rows do you want to see?',
+  })
   @IsOptional()
   @Min(0)
   // Transformar
   @Type(() => Number) // enableImplicitConversions: true
   limit?: number;
 
+  @ApiProperty({
+    default: 0,
+    description: 'How many rows do you want to skip?',
+  })
   @IsOptional()
   @Min(0)
   @Type(() => Number) // enableImplicitConversions: true
