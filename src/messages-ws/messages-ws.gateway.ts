f54216d3ddfc0f9ff2cf6f7a61247860c7a0780e
thezxzx <oscarmiguelayalaruelas@gmail.com>
2023-06-11 14:42:23 -0700

    Obtener clientes conectados

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -2,24 +2,33 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
+  @WebSocketServer() wss: Server;
+
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
   handleConnection(client: Socket) {
     this.messagesWsService.registerClient(client);
 
-    console.log({ conectados: this.messagesWsService.getConnectedClients() });
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
   }
   handleDisconnect(client: Socket) {
     this.messagesWsService.removeClient(client.id);
 
-    console.log({ conectados: this.messagesWsService.getConnectedClients() });
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
   }
 }
