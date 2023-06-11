f9e4a083fa50e87d2d13e738262bf526d7886b0c
thezxzx <oscarmiguelayalaruelas@gmail.com>
2023-06-11 16:07:25 -0700

    preparar cliente para enviar jwt y recibirlo en los headers

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -18,6 +18,8 @@ export class MessagesWsGateway
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
   handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string;
+    console.log({ token });
     this.messagesWsService.registerClient(client);
 
     this.wss.emit(
