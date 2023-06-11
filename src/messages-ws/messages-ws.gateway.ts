32c0f162c7a6a379dddc6831979a3a35aaf184b7
thezxzx <oscarmiguelayalaruelas@gmail.com>
2023-06-11 03:19:25 -0700

    server - mantener identifiacados los clientes

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -13,9 +13,13 @@ export class MessagesWsGateway
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
   handleConnection(client: Socket) {
-    console.log('Cliente conectado:', client.id);
+    this.messagesWsService.registerClient(client);
+
+    console.log({ conectados: this.messagesWsService.getConnectedClients() });
   }
   handleDisconnect(client: Socket) {
-    console.log('Cliente desconectado:', client.id);
+    this.messagesWsService.removeClient(client.id);
+
+    console.log({ conectados: this.messagesWsService.getConnectedClients() });
   }
 }
